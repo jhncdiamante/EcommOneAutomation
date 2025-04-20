c1cbb45580e7195b0e9a7661b5d4b48b27f667ee
johncarlodeveloepr <2023100305@bulsu.edu.ph>
2025-04-20 10:56:29 +0800

    working ver

--- App.py
+++ App.py
@@ -2,15 +2,17 @@ from One import OneEcomm
 from WebDriverManager import Driver
 from datetime import datetime
 import pandas as pd
-import time
 
-INPUT_FILE_PATH = "input.csv"
-if INPUT_FILE_PATH.endswith(".csv"):
+INPUT_FILE_PATH = "input.csv" # <<<< INPUT FILE TYPE MUST MATCH THE OUTPUT FILE TYPE
+OUTPUT_FILE_PATH = "output.csv"
+if INPUT_FILE_PATH.endswith(".csv") and OUTPUT_FILE_PATH.endswith(".csv"):
     input_file = pd.read_csv(INPUT_FILE_PATH)
-elif INPUT_FILE_PATH.endswith(".xlsx"):
+elif INPUT_FILE_PATH.endswith(".xlsx") and OUTPUT_FILE_PATH.endswith(".xlsx"):
     input_file = pd.read_excel(INPUT_FILE_PATH)
 else:
     raise ValueError("Incorrect file format.")
+
+
 
 booking_numbers = input_file.iloc[:, 0].dropna().unique().tolist()
 
@@ -20,28 +22,18 @@ base_url = "https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking"
 #booking_number = "DVOF00401402"
 datalist = []
 MATCH_RULES = {
-        "gate in": {
-            "keyword": "gate in",
-            "type": "first"
-        },
-        "departure": {
-            "keyword": "departure",
-            "type": "first"
-        },
-        "arrival": {
-            "keyword": "arrival",
-            "type": "last"
-        },
-        "discharging": {
-            "keyword": "discharge",
-            "type": "last"
-        },
-        "gate out": {
-            "keyword": "gate out",
-            "type": "last"
-        }
+        "gate in": 0,
+        "departure": 0,
+        "arrival": 1,
+        "discharging": 1,
+        "gate out": 1
     }
+table_data = {key: None for key in MATCH_RULES}
+print(table_data)
+exit()
+
 One = OneEcomm(driver, base_url)
+
 for booking_number in booking_numbers:
     One.open_page()
     
@@ -60,12 +52,13 @@ for booking_number in booking_numbers:
         for status, date in data.items():
             for column_name, rule in MATCH_RULES.items():
                 if column_name in status:
-                    if rule["type"] == "first" and table_data[column_name] is None:
+                    if rule == 0 and table_data[column_name] is None:
                         table_data[column_name] = date
+                        
                         if column_name == 'departure':
                             vessel_and_number = status.split("' ")[0]
                             table_data['departure vessel and number'] = vessel_and_number[1:].upper()
-                    elif rule["type"] == "last":
+                    elif rule == 1:
                         table_data[column_name] = date
                         if column_name == 'arrival':
                             vessel_and_number = status.split("' ")[0]
@@ -78,7 +71,7 @@ for booking_number in booking_numbers:
     #One._driver.refresh()
 
 df = pd.DataFrame(datalist)
-df.to_csv(r"OUTPUT.csv", index=False)
+df.to_csv(OUTPUT_FILE_PATH, index=False)
 print("Done.")
 
 
